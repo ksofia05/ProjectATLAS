9d26ae858917bdf59d353e5cfdcdeab0f06cc528
LuisitoAlca <yundaluis4@gmail.com>
2025-04-18 01:42:12 -0500

    Adelanto del registro e inicio

--- backend/autenticacion/urls.py
+++ backend/autenticacion/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from autenticacion import views
 
 urlpatterns = [
-
-
+    path('login/', views.login_view, name='login'),  # Ruta para el inicio de sesión
+    path('register/', views.register, name='register'),  # Primer paso del registro
+    path('register2/', views.register2, name='register2'),  # Segundo paso del registro
 ]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,3 +137,6 @@ CORS_ALLOWED_ORIGINS = []
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
+
+
+STATIC_URL = '/static/'
