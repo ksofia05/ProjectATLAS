8c1150da7ec06812e9356ba2284b7e3f4885f5aa
LuisitoAlca <yundaluis4@gmail.com>
2025-05-07 22:54:01 -0500

    Creacion de las rutas de Autenticacion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+//Importaciones de Karen (Landing page)
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/navbar';
@@ -8,6 +9,17 @@ import SobreNosotros from './pages/sobreNosotros'; // ruta quienes somos
 import PoliticasPrivacidad from './components/politicasPrivacidad'; // ruta politica de privacidad
 import TerminosCond from './components/terminosCond'; // ruta terminos y condiciones
 
+//Importaciones de Luis (Autenticacion)
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Register2 from './pages/Register2';
+import PasswordRecovery from './pages/PasswordRecovery';
+import PasswordReset from './pages/PasswordReset';
+import ResendLink from './pages/ResendLink';
+import EmailRecovery from './pages/EmailRecovery';
+import Terms from './pages/Terms';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import Simulation from './pages/Simulation';
 
 const App = () => {
   return (
@@ -20,6 +32,18 @@ const App = () => {
         <Route path="/registrarse" element={<Resgistrarse />} />
         <Route path="/politica-de-privacidad" element={<PoliticasPrivacidad />} />
         <Route path="/terminos-y-condiciones" element={<TerminosCond />} />
+
+
+        <Route path="/iniciar-sesion" element={<Login />} />
+        <Route path="/registrarse" element={<Register />} />
+        <Route path="/registrarse-paso-2" element={<Register2 />} />
+        <Route path="/recuperar-contrasena" element={<PasswordRecovery />} />
+        <Route path="/crear-contrasena" element={<PasswordReset />} />
+        <Route path="/reenviar-enlace" element={<ResendLink />} />
+        <Route path="/email-recuperacion" element={<EmailRecovery />} />
+        <Route path="/terminos" element={<Terms />} />
+        <Route path="/politica-de-privacidad" element={<PrivacyPolicy />} />
+        <Route path="/simulacion" element={<Simulation />} />
         </Routes>
     </BrowserRouter>
   );
