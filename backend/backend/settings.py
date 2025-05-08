956cfc583c9a909c7a410fdff8b694642a9c4c41
ksofia05 <karensofializcanotorres@gmail.com>
2025-05-08 15:59:21 -0500

    se añade la parte de la api para backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [BASE_DIR / "backend" / "autenticacion" / "static"]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #cords headers
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
