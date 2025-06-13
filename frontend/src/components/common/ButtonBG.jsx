a40fd9795975d872f50b3134f22ac4d67676fce8
moralesJuan22 <moralito2211@gmail.com>
2025-06-12 20:12:13 -0500

    Add Modal cancel in ProfilePage

--- frontend/src/components/common/ButtonBG.jsx
+++ frontend/src/components/common/ButtonBG.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-const ButtonBG = ({ children, onClick, className = "" }) => (
-  <button
-    onClick={onClick}
-    className={`ml-auto bg-gradient-to-l from-[#181825] to-[#232335] text-gray-200 px-3 py-1 rounded-lg whitespace-nowrap hover:bg-[#7c2ae8] hover:text-white transition ${className}`}
-  >
-    {children}
-  </button>
-);
-
-export default ButtonBG;
+export default function ButtonGray({ className = "", ...props }) {
+  return (
+    <button
+      className={`px-4 py-2 rounded ${className}`}
+      {...props}
+    />
+  );
+}
