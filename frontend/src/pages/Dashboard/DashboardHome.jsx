dd283703ad6dc57df27a0a85af1c6cbbb5426a34
ksofia05 <karensofializcanotorres@gmail.com>
2025-09-04 11:13:57 -0500

    avances del responsive

--- frontend/src/pages/Dashboard/DashboardHome.jsx
+++ frontend/src/pages/Dashboard/DashboardHome.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../../components/layout/Sidebar";
 import Navbar from "../../components/layout/Navbar";
 import { Outlet, useParams } from "react-router-dom";
@@ -34,6 +34,15 @@ const DashboardLayout = () => {
     }
   }, [projectId, user, isValidating, hasAccess, fetchProjectInfo, fetchCollaborators]);
 
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
   // Debido a complicaciones, tuve que obtener el nombre del usuario de diferentes maneras xd
   const firstName =
     user?.user_metadata?.nombre?.split(" ")[0] ||
@@ -101,8 +110,8 @@ const DashboardLayout = () => {
 
   return (
     <div className="min-h-screen bg-slate-950">
-      <Sidebar showLogo={true} menuItems={getMenuItems()} footerLinks={true} />
-      <div className="ml-72 flex flex-col min-h-screen">
+      <Sidebar showLogo={true} menuItems={getMenuItems()} footerLinks={true} isOpen={isSidebarOpen} onClose={closeSidebar}/>
+      <div className="sm:ml-0 md:ml-0 lg:ml-72 flex flex-col min-h-screen">
         {/* Navbar flotante con margen igual al contenido */}
         <div className="sticky top-0 z-10 pt-6 bg-slate-950">
           <div className="px-8">
@@ -111,12 +120,14 @@ const DashboardLayout = () => {
               showUpgradeButton={false}
               title={`Bienvenido/a${firstName ? " " + firstName : ""}`}
               subtitle="Aquí, Las estadísticas de esta semana!"
+              onSidebarToggle={toggleSidebar}
+              isSidebarOpen={isSidebarOpen}
             />
           </div>
         </div>
         {/* Unico apartado donde hay scroll de manera general */}
         <div className="flex-1 px-8 pb-6 pt-4 overflow-y-auto">
-          <Outlet />
+          <Outlet />  
         </div>
       </div>
     </div>
